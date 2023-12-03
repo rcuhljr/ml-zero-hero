32155e4323a89725556fadc015aa79d1f3b6a5b2
Robert Uhl <rcuhljr@gmail.com>
2023-12-03 14:17:33 -0500

    wrapup pt2

--- micrograd_learner.py
+++ micrograd_learner.py
@@ -1,4 +1,5 @@
 import math
+from typing import Any
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,7 +20,7 @@ class Value:
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
-        self.label = label
+        self.label = label if label else _op
 
     def __repr__(self) -> str:
         if len(self._prev) > 0:
@@ -39,7 +40,7 @@ class Value:
 
     def __radd__(self, other):
         out = self + other
-        out.label= '?'
+        out.label= '+'
         return out
 
     def __mul__(self, other):
@@ -59,17 +60,17 @@ class Value:
 
     def __truediv__(self, other):
         out = self * other**-1
-        out.label= '?'
+        out.label= '/'
         return out
 
     def __neg__(self):
         out = self * -1
-        out.label= '?'
+        out.label= '-'
         return out
 
     def __sub__(self, other):
         out = self + (-other)
-        out.label= '?'
+        out.label= '-'
         return out
 
     def __pow__(self, other):
@@ -155,3 +156,102 @@ dbla = Value(1.0, label='a')
 newb = 1 + dbla + dbla; newb.label ='b'
 newb.backward()
 print(newb)
+
+
+# so this kept annoying me, I need to look at his actual code to see if this is fixed or find out if it's not a bug.
+# I think it's debatably a bug, but it's not the purpose of the library so it really probably doesn't matter.
+for i in range(10):
+    print(Value(1.0) / Value(2.0))
+
+
+# import torch
+
+# x1 = torch.Tensor([2.0]).double()                ; x1.requires_grad = True
+# x2 = torch.Tensor([0.0]).double()                ; x2.requires_grad = True
+# w1 = torch.Tensor([-3.0]).double()               ; w1.requires_grad = True
+# w2 = torch.Tensor([1.0]).double()                ; w2.requires_grad = True
+# b = torch.Tensor([6.8813735870195432]).double()  ; b.requires_grad = True
+# n = x1*w1 + x2*w2 + b
+# o = torch.tanh(n)
+
+# print(o.data.item())
+# o.backward()
+
+# print('---')
+# print('x2', x2.grad.item())
+# print('w2', w2.grad.item())
+# print('x1', x1.grad.item())
+# print('w1', w1.grad.item())
+
+import random
+
+class Neuron:
+
+    def __init__(self, nin) -> None:
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1,1))
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        act = sum((wi*xi for wi, xi in zip(self.w, *args)),  self.b)
+        out = act.tanh()
+        return out
+
+    def parameters(self):
+        return self.w + [self.b]
+
+class Layer:
+
+    def __init__(self, nin, nout) -> None:
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        outs = [n(*args) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+
+class MLP:
+
+    def __init__(self, nin, nouts) -> None:
+        sz = [nin]+nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+x = [2.0, 3.0, -1.0]
+n = MLP(3, [4, 4, 1])
+# print(n(x).data)
+
+xs = [
+  [2.0, 3.0, -1.0],
+  [3.0, -1.0, 0.5],
+  [0.5, 1.0, 1.0],
+  [1.0, 1.0, -1.0],
+]
+ys = [1.0, -1.0, -1.0, 1.0]
+
+for k in range(100):
+    ypred = [n(x) for x in xs]
+    loss = sum([(yout - ygt)**2 for ygt, yout in zip(ys, ypred)])
+    if k % 10 == 0:
+        print(f"{k}:{loss.data:.4f}")
+
+    for p in n.parameters():
+        p.grad = 0.0
+    loss.backward()
+    if loss.data < 0.0001:
+        break
+
+    for p in n.parameters():
+        p.data += -0.1 * p.grad
+
+print("final")
+for y in ypred:
+    print(y.data)
